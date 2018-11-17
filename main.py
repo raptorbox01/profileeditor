44aee658918143d61f0cba79e5023958b83f0011
Notiel(Julia Salnikova) <agereth@gmail.com>
2018-11-18 02:08:09 +0300

    +controls for common.ini +tab widget +delete of effect, sequencer, config

--- main.py
+++ main.py
@@ -3,91 +3,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtGui import QPixmap
 import design
-import json
+from Auxledsdata import *
 
 from PyQt5.QtGui import QIcon
-
-class AuxEffects:
-
-    def __init__(self):
-        self.data = dict()
-
-
-    def AddEffect(self, name: str):
-        self.data[name] = list()
-
-    def GetEffectsList(self):
-        return self.data.keys()
-
-
-    def GetUsedLedsList(self, effect: str):
-        ledsused = list()
-        try:
-            sequencers = self.data[effect]
-        except KeyError:
-            print("No such effect % s" % effect)  #ToDo add logging
-        for sequencer in sequencers:
-            ledsused.extend(sequencer['Config'])
-        return ledsused
-
-    def GetJsonToFile(self, filename):
-        text = json.dumps(self.data)
-        text = text.replace(r'"', "")
-        text = text[1:-1]
-        f = open(filename, "w")
-        f.write(text)
-
-
-    def CreateSequencer(self, effect: str, leds: list):
-        try:
-            sequencers = self.data[effect]
-        except KeyError:
-            print("No such effect % s" % effect)  #ToDo add logging
-        sequencers.append({"Config": leds, "Sequence": []})
-        print(self.data)
-
-
-    def CreateStep(self, effect:str, number:int, name: str, brightnesses:list, wait: int, smooth: int):
-        try:
-            sequence = self.data[effect][number]['Sequence']
-            step = dict()
-            if name:
-                step['Name'] = name
-            step['Brightness'] = brightnesses
-            if wait > 0:
-                step['Wait'] = wait
-            if smooth > 0:
-                step['Smooth'] = smooth
-            sequence.append(step)
-        except (KeyError, IndexError):
-            print("Cannot add step to %i sequencer of %s effect" % (number, effect)) #ToDo add Logging
-        print(self.data)
-
-
-    def CreateRepeatStep(self, effect: str, number: int, startstep: str, count: str):
-        try:
-            sequence = self.data[effect][number]['Sequence']
-            step = {'Repeat': {'StartingFrom': startstep}}
-            if count != 'forever':
-                count = int(count)
-            step['Repeat']['Count'] = count
-        except (KeyError, IndexError):
-            print("Cannot add step to %i sequencer of %s effect" % (number, effect)) #ToDo add Logging
-        print(self.data)
-
-
-    def GetStepsList(self, effect: str, number: int):
-        steps_list = list()
-        try:
-            sequence = self.data[effect][number]['Sequence']
-            for step in sequence:
-                if 'Name' in step.keys():
-                    steps_list.append(step['Name'])
-            return steps_list
-        except (KeyError, IndexError):
-            print("Cannot get steps of %i sequencer of %s effect" % (number, effect))  # ToDo add Logging
-            return []
-
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -127,6 +45,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.BtnAddSequence.clicked.connect(self.AddSequence)
         self.BtnAddStep.clicked.connect(self.AddStep)
         self.BtnRepeat.clicked.connect(self.AddRepeatStep)
+        self.BtnDelete.clicked.connect(self.DeleteItem)
 
         self.TxtName.textChanged[str].connect(self.NameChanged)
         self.CBLedsAll.clicked.connect(self.CheckAllLeds)
@@ -164,12 +83,40 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             if item_text == name:
                 self.TrStructure.setCurrentItem(item)
 
-    def TreeItemChanged(self, current):
 
+    def GetItemType(self, item: QtWidgets.QTreeWidgetItem):
         root = self.TrStructure.invisibleRootItem()
         children = [root.child(i) for i in range(root.childCount())]
+        if item in children:
+            return "Effect"
+        parent = item.parent()
+        if parent in children:
+            return "Sequencer"
+        grandpa = parent.parent()
+        if grandpa in children:
+            return "Config"
+        return "Step"
 
-        if current in children:
+
+    def StepControlsDisable(self):
+        self.SpinWait.setEnabled(False)  # all step controls are disabled
+        self.SpinSmooth.setEnabled(False)
+        self.ComboRepeat.setEnabled(False)
+        self.ComboRepeat.clear()
+        self.ComboCount.setEnabled(False)
+        self.BtnAddStep.setEnabled(False)
+        self.BtnRepeat.setEnabled(False)
+        self.TxtStepName.setEnabled(False)
+        self.BtnRepeat.setEnabled(False)
+        for label in self.steplabels:
+            label.setEnabled(False)
+        for led in self.stepbrightness:
+            led.setEnabled(False)
+
+    def TreeItemChanged(self, current):
+
+        item_type = self.GetItemType(current)
+        if item_type == 'Effect':
             self.BtnAddSequence.setEnabled(True)  # if item is toplevel we can add sequencer
             name = current.text(0)
 
@@ -183,19 +130,8 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     self.ledsdict[led].setChecked(True)
             self.CBLedsAll.setEnabled(True)
             self.CBLedsAll.setChecked(False)
+            self.StepControlsDisable()
 
-            self.SpinWait.setEnabled(False)      #all step controls are disabled
-            self.SpinSmooth.setEnabled(False)
-            self.ComboRepeat.setEnabled(False)
-            self.ComboRepeat.clear()
-            self.ComboCount.setEnabled(False)
-            self.BtnAddStep.setEnabled(False)
-            self.BtnRepeat.setEnabled(False)
-            self.TxtStepName.setEnabled(False)
-            for label in self.steplabels:
-                label.setEnabled(False)
-            for led in self.stepbrightness:
-                led.setEnabled(False)
 
         else:
             self.BtnAddSequence.setEnabled(False)  #for not top-level items sequencer and leds are not available
@@ -204,7 +140,7 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             self.CBLedsAll.setEnabled(False)
             self.CBLedsAll.setChecked(False)
 
-            if current.parent() in root.children() or current.parent().parent() in root.children():
+            if item_type != 'Step':
                 self.SpinWait.setEnabled(True)  # all step controls are enabled, but only available leds controls enabled
                 self.SpinSmooth.setEnabled(True)
                 self.ComboRepeat.setEnabled(True)
@@ -214,12 +150,15 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.TxtStepName.setEnabled(True)
                 self.EnableLedsList()
                 self.GetStepsNames()
+            else:
+                self.StepControlsDisable()
 
         self.SpinWait.setValue(0)
         self.SpinSmooth.setValue(0)
         self.TxtStepName.clear()
         for led in self.stepbrightness:
             led.setValue(0)
+        self.BtnDelete.setEnabled(True)
 
 
 
@@ -336,6 +275,43 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         step_text = "Repeat: StartFrom: %s, Count: %s" % (startstep, count)
         step_item = QtWidgets.QTreeWidgetItem([step_text])
         current.child(0).addChild(step_item)
+
+
+    def DeleteItem(self):
+        current = self.TrStructure.currentItem()
+        current_name = current.text(0)
+        current_type = self.GetItemType(current)
+        if current_type == 'Effect':
+            self.data.DeleteItem(current_name, [])
+            root = self.TrStructure.invisibleRootItem()
+            root.removeChild(current)
+        else:
+            if current_type == 'Sequencer':
+                number = int(current_name.replace("Sequencer", "")) - 1
+                self.data.DeleteItem(number, [current.parent().text(0)])
+                current.parent().removeChild(current)
+            if current_type == 'Config':
+                parent = current.parent()
+                grandpa = parent.parent()
+                effect = grandpa.text(0)
+                number = int(parent.text(0).replace("Sequencer", "")) - 1
+                self.data.DeleteItem("Config", [effect, number])
+                if parent.childCount() == 1:
+                    grandpa.removeChild(parent)
+                    self.data.DeleteItem(number, [effect])
+                else:
+                    parent.removeChild(current)
+            if current_type == 'Step':
+                parent = current.parent()
+                grandpa = parent.parent()
+                ancestor = grandpa.parent()
+                number = int(grandpa.text(0).replace("Sequencer", "")) - 1
+                effect = ancestor.text(0)
+
+
+
+
+
 
 
     def CheckAllLeds(self):
